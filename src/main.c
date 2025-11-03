147d6b5779c1495496b482fb0aee1afed85d9bd3
fraxek <mwfraczek@icloud.com>
2025-11-02 21:48:20 -0500

    feature usart and refactor sensor read/print loop

--- src/main.c
+++ src/main.c
@@ -3,28 +3,43 @@
 #include "utils.h"
 #include "i2c.h"
 #include "bmp390.h"
-
-uint8_t bmpid; 
-uint8_t bmpstatus;
-uint8_t bmpdata[6];
+#include "usart.h"
 
 int main(void) {
+	// Variable declarations
+	uint8_t bmpstatus;
+	volatile float temperature_c;
+	volatile float pressure_pa;
+	bmp_coeff_t cal_raw; 	// raw coeffs
+	bmp_par_t cal;		// converted coeffs
+	bmp_raw_t raw;		// raw readings
+
 	// Setup STM32 peripherals 
 	rcc_enable();
 	gpio_config();
 	tim2_enable();
-
-	// Setup I2C1 peripheral 
 	i2c1_init();
+	usart2_init();
 
-	// Function Calls
-	if (bmp390_setup() != 0)
-		led_blink_fast();
-	if (bmp390_chipid(&bmpid) != 0)
-		led_blink_fast();
-	if (bmp390_status(&bmpstatus) != 0)
-		led_blink_fast();
-	bmp390_data(bmpdata);
-	while (1) 
+	// Setup and wait for ready sensor 
+	if (bmp390_setup() != 0) led_blink_fast();
+	do {
+		if (bmp390_status(&bmpstatus) != 0)
+			led_blink_fast(); 
+	} while (bmpstatus != 0x70);
+	
+	// Read and Convert Sensor Calibration Coefficients 
+	bmp390_coeffdata(&cal_raw);
+        bmp390_coeffconvert(&cal_raw, &cal);
+	
+	// Print temp & pressure values via UART 
+	while (1) {
+		bmp390_rawdata(&raw);
+        	temperature_c = bmp390_temp(raw.raw_temperature, &cal);
+        	pressure_pa = bmp390_pressure(raw.raw_pressure, &cal);
+
+		print_bmp390_readings(temperature_c, pressure_pa);
+		
 		led_blink_slow();
+	}
 }
